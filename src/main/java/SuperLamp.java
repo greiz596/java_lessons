846ac5ed2cce8466e2d484480a9d6b75d59f9567
greiz <r.romadin2010@yandex.ru>
2024-05-10 07:57:54 +0300

    yandex_sprint_6

---
+++ src/main/java/SuperLamp.java
@@ -0,0 +1,7 @@
+public class SuperLamp implements Lamp {
+    // метод включает свет лампочкой «Суперлампа»
+    @Override
+    public void switchLightOn() {
+        System.out.println("Я включаю лампочку марки «Суперлампа» и сияю на ура!");
+    }
+}
