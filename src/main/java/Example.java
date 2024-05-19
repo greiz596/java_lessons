0430fd8d11bd0d7d3e1f0b177f2db4a9a7d0eb55
greiz <r.romadin2010@yandex.ru>
2024-05-19 13:39:28 +0300

    спринт 5, первые два раздела

--- src/main/java/Example.java
+++
@@ -1,9 +0,0 @@
-public class Example {
-    public static void main(String[] args) {
-// чтобы заменить лампочку, достаточно вызвать new SuperLamp(),
-// остальной код не изменится
-        Lamp horizonLamp = new SuperLamp();
-        Fridge fridge = new Fridge(horizonLamp);
-        fridge.openDoor();
-    }
-}
--- src/main/java/HorizonLamp.java
+++
@@ -1,7 +0,0 @@
-public class HorizonLamp implements Lamp {
-    // метод включает свет лампочкой «Горизонт»
-    @Override
-    public void switchLightOn() {
-        System.out.println("Я включаю лампочку марки «Горизонт»");
-    }
-}
